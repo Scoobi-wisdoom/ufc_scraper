bdbc18ad9cb4df02e9a63b4f64f0cb68b5d66ded
Scoobi-wisdoom <grevod@gmail.com>
2021-02-03 02:42:08 +0900

    fighters stats are done with pandas

--- time_n_fighters_to_sql.py
+++ time_n_fighters_to_sql.py
@@ -6,6 +6,7 @@ import json
 import os
 import requests
 import string
+import re
 
 # MYSQL 에 연결
 with open("db_name.txt", "r") as f:
@@ -113,10 +114,10 @@ results = results.drop(['defeat_red', 'defeat_blue'], axis=1)
 with engine.connect() as con:
     ## results table
     results.to_sql(con=con, name='results', if_exists='replace', index=True,
-                     dtype={
-                            None: sqlalchemy.types.INT,
-                            'result_r_b': sqlalchemy.types.VARCHAR(length=255)
-                            }
+                   dtype={
+                       None: sqlalchemy.types.INT,
+                       'result_r_b': sqlalchemy.types.VARCHAR(length=255)
+                   }
                    )
     con.execute('ALTER TABLE `results` ADD PRIMARY KEY (`index`);')
     con.execute('ALTER TABLE `results` CHANGE `index` `result_id` INT;')
@@ -126,11 +127,31 @@ with open('fighter_url.json', 'r') as f:
     json_data = json.loads(f.read())
     fighter_url = pd.DataFrame(json_data)
 
+## 역대 ufc 경기에 참여했던 인원을 순서대로 list 화 하고, 이의 url 을 담는다.
 fighter_url_only_list = []
 for red_url, blue_url in fighter_url[['fighter_url_red', 'fighter_url_blue']].itertuples(index=False):
     fighter_url_only_list.append(red_url)
     fighter_url_only_list.append(blue_url)
     fighter_url_only_list = list(dict.fromkeys(fighter_url_only_list))
+
+## ufcstats 웹 사이트에서 선수 목록을 스크래핑한다.
+# headers = {'User-agent': 'Mozilla/5.0'}
+# url_list = []
+# for char in string.ascii_lowercase:
+#     url = 'http://www.ufcstats.com/statistics/fighters?char={}&page=all'.format(char)
+#     sauce = requests.get(url, headers=headers)
+#     soup = BeautifulSoup(sauce.text, 'lxml')
+#     tr = soup.find_all('tr', class_='b-statistics__table-row')
+#     for r in tr:
+#         try:
+#             url_list.append(r.find('a').attrs['href'])
+#         except AttributeError:
+#             continue
+#     print(char, 'is done')
+
+## 이제 경기를 했던 선수들의 목록 + 선수 목록을 한다. 당연히 겹치는 건 제외한다.
+# fighter_url_only_list = fighter_url_only_list + url_list
+# fighter_url_only_list = list(dict.fromkeys(fighter_url_only_list))
 
 ## fighter 페이지를 html 파일로 저장한다.
 headers = {'User-agent': 'Mozilla/5.0'}
@@ -144,9 +165,124 @@ for url in fighter_url_only_list:
     i += 1
     print(i, 'is done out of', len(fighter_url_only_list))
 
-for char in string.ascii_lowercase:
-    url = 'http://www.ufcstats.com/statistics/fighters?char={}&page=all'.format(char)
 
-# file_soup = BeautifulSoup(open(path + '1.html'), 'html.parser')
-# file_soup.find(class_='b-fight-details__persons clearfix').find(class_='b-fight-details__person').find('a').attrs[
-#     'href']
+## fighter Table 을 MYSQL 에 입력한다.
+fighter_path = 'fighter_html/'
+s = os.listdir(path=fighter_path)
+s.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]))
+
+fighter_columns = ['fighter_name', 'fighter_nickname', 'height', 'weight',
+                   'reach', 'stance', 'dob', 'record_win', 'record_loss',
+                   'record_draw', 'record_nc', 'record_sl_pm', 'record_str_acc', 'record_sa_pm',
+                   'record_str_def', 'record_td_avg', 'record_td_acc', 'record_td_def', 'record_sub_avg'
+                   ]
+
+fighters_df = pd.DataFrame(columns=fighter_columns)
+### feet and inches 를 centimeters 로 변환하는 함수를 정의한다.
+def to_cm(string):
+    import re
+    string_split = string.split("'")
+    feet = 0
+    if len(string_split) > 1:
+        feet = re.search(r'\d+', string_split[0]).group()
+    try:
+        inches = re.search(r'\d+', string_split[-1]).group()
+    except:
+        return None
+
+    feet = float(feet)
+    inches = float(inches)
+    centimeters = feet * 30.48 + inches * 2.54
+
+    return centimeters
+
+i = 0
+for html in s:
+    i += 1
+    fighter_soup = BeautifulSoup(open(fighter_path+html), 'html.parser')
+    fighter_name = fighter_soup.find('span', class_='b-content__title-highlight').text.strip()
+    fighter_nickname = fighter_soup.find('p', class_='b-content__Nickname').text.strip()
+
+    ## physique
+    physique_li_list = fighter_soup.find('div', class_='b-list__info-box b-list__info-box_style_small-width js-guide').find_all('li')
+    physique_list = []
+    for physique in physique_li_list:
+        text = physique.find_all(text=True, recursive=False)
+        physique_list.append(''.join(t.strip() for t in text))
+    ### to be converted into metric
+    height = to_cm(physique_list[0])
+    ### lbs
+    try:
+        weight = float(re.search(r'\d+', physique_list[1]).group())
+    except:
+        weight = None
+    ### to be converted into metric
+    reach = to_cm(physique_list[2])
+    stance = physique_list[3]
+    dob = physique_list[4]
+
+    ## record
+    record = str.lower(fighter_soup.find('span', class_='b-content__title-record').text.strip())
+    record = record.replace('record:', '').strip()
+    record_win = record.split('-')[0]
+    record_loss = record.split('-')[1]
+    record_draw = record.split('-')[2]
+    record_nc = 0
+    ### record 에 nc 가 있으면 draw, nc 값을 다르게 split 해야 한다.
+    draw_nc = record.split('-')[2]
+    if draw_nc.find('nc') > -1:
+        record_draw = re.findall(r'\d+', draw_nc)[0]
+        record_nc = re.findall(r'\d+', draw_nc)[-1]
+    # record_win = int(record_win)
+    # record_loss = int(record_loss)
+    # record_draw = int(record_draw)
+    # record_nc = int(record_nc)
+
+    ## career statistics
+    stats_ul_list = fighter_soup.find('div', class_='b-list__info-box-left clearfix').find_all('ul')
+    stat1_list = []
+    for stat in stats_ul_list[0].find_all('li'):
+        text = stat.find_all(text=True, recursive=False)
+        stat1_list.append(''.join(t.strip() for t in text))
+    record_sl_pm = stat1_list[0]
+    record_str_acc = stat1_list[1]
+    record_sa_pm = stat1_list[2]
+    record_str_def = stat1_list[3]
+
+    stat2_list = []
+    for stat in stats_ul_list[-1].find_all('li'):
+        text = stat.find_all(text=True, recursive=False)
+        stat2_list.append(''.join(t.strip() for t in text))
+    record_td_avg = stat2_list[1]
+    record_td_acc = stat2_list[2]
+    record_td_def = stat2_list[3]
+    record_sub_avg = stat2_list[4]
+
+    a_row = [fighter_name, fighter_nickname, height, weight,
+             reach, stance, dob, record_win, record_loss,
+             record_draw, record_nc, record_sl_pm, record_str_acc, record_sa_pm,
+             record_str_def, record_td_avg, record_td_acc, record_td_def, record_sub_avg
+             ]
+
+    fighters_df_sub = pd.DataFrame([a_row], columns=fighters_df.columns)
+    fighters_df = pd.concat([fighters_df, fighters_df_sub])
+
+    print(i,'is done out of', len(s))
+
+fighters_df.reset_index(drop=True, inplace=True)
+## data type 을 column 별로 정한다.
+### numeric1
+fighters_df[['height', 'weight', 'reach']] = fighters_df[['height', 'weight', 'reach']].apply(pd.to_numeric)
+### date
+fighters_df['dob'] = pd.to_datetime(fighters_df['dob'].str.replace('--', 'NaT'))
+### numeric2
+fighters_df[['record_win', 'record_loss', 'record_draw', 'record_nc']] = \
+    fighters_df[['record_win', 'record_loss', 'record_draw', 'record_nc']].apply(pd.to_numeric)
+### numeric3 without percent signs
+fighters_df[['record_sl_pm', 'record_sa_pm', 'record_td_avg', 'record_sub_avg']] = \
+    fighters_df[['record_sl_pm', 'record_sa_pm', 'record_td_avg', 'record_sub_avg']].apply(pd.to_numeric)
+### numeric4 with percent signs
+fighters_df[['record_str_acc', 'record_str_def', 'record_td_acc', 'record_td_def']] =  \
+    fighters_df[['record_str_acc', 'record_str_def', 'record_td_acc', 'record_td_def']].apply(lambda x: x.str.strip('%')).apply(pd.to_numeric) / 100
+
+fighters_df.to_json('fighter_to_sql.json')
